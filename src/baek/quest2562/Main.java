282ce0ce7a60cd99e444a4d1065cce2cf0c8ab62
diaryhub <kjw991953@gmail.com>
2022-01-04 14:20:57 +0900

    2562

--- src/baek/quest2562/Main.java
+++ src/baek/quest2562/Main.java
@@ -1,10 +1,34 @@
 package baek.quest2562;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int [] integer = new int[9];
+		
+		for(int i=0;i<9;i++) {
+			
+			integer[i]=sc.nextInt();
+			
+		}
+		
+		int max = Integer.MIN_VALUE;
+		int count = 1;
+		
+		for(int i=0;i<9;i++) {
+			
+			if(max<integer[i]) {
+				max=integer[i];
+				count = i+1;
+			}
+		}
+		System.out.println(max+"\n"+count);
+		
+		
+		
 	}
 
 }
