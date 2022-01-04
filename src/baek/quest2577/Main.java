d974c29fb90cacac2dfaf3c0273dfd5f80fb67aa
diaryhub <kjw991953@gmail.com>
2022-01-04 14:33:22 +0900

    wait

---
+++ src/baek/quest2577/Main.java
@@ -0,0 +1,21 @@
+package baek.quest2577;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int x=a*b*c;
+		
+		
+		
+	}
+
+}
