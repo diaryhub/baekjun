766f4964787d296ff2ddc01d0be78ce1d5410ac1
diaryhub <kjw991953@gmail.com>
2022-01-04 23:59:42 +0900

    3052answer

--- src/baek/quest3052/Main.java
+++ src/baek/quest3052/Main.java
@@ -16,21 +16,25 @@ public class Main {
 			
 		a = sc.nextInt();
 		
-		array[i]=a%42;
+		array[i]= a % 42;
 			
 		}
 		
 		int [] array2 = new int [42];
 		
+		for(int i=0;i<42;i++) {
+			
+			array2[i]=0;
+		
+		}
+		
 		for(int i=0;i<10;i++) {
 			
 			for(int j=0;j<42;j++) {
 				
-				array2[j]=0;
-				
 				if(j==array[i]) {
 					
-					array2[j]+=1;
+					array2[j]=array2[j]+1;	
 					
 				}
 				
@@ -38,7 +42,19 @@ public class Main {
 			
 		}
 		
-		System.out.println();
+		int print = 10;
+		
+		for(int i=0;i<42;i++) {
+			
+			if(array2[i]>1) {
+				
+				print= print -array2[i] + 1;
+				
+			}
+			
+		}
+		
+		System.out.println(print);
 		
 	}
 
