c82a11d5c8b0daa772dff0c43a39fee27a1212da
diaryhub <kjw991953@gmail.com>
2022-01-05 17:53:10 +0900

    15596 answer

---
+++ src/baek/quest15596/Test.java
@@ -0,0 +1,19 @@
+package baek.quest15596;
+
+public class Test {
+	
+    long sum(int[] a) {
+        
+    	long ans = 0;
+        
+    	for(int i=0;i<a.length;i++) {
+    		
+    		ans +=a[i];
+    		
+    	}
+    	
+        return ans;
+        
+    }
+    
+}
