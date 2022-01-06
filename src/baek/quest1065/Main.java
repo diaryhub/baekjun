0843395ce33d402c055ce5d1f300bcfd09cefc27
diaryhub <kjw991953@gmail.com>
2022-01-06 18:29:01 +0900

    1065 answer

--- src/baek/quest1065/Main.java
+++ src/baek/quest1065/Main.java
@@ -1,36 +1,37 @@
 package baek.quest1065;
 
 import java.io.*;
-
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		int n = Integer.parseInt(br.readLine());
 		int ans = 0;
-		if(n<100)
+		if(n<100) {
 			ans = n;
+			bw.write(""+ans);
+			bw.flush();
+			bw.close();
+		}
 		else
 		{
 			ans = 99;
-			
 			for(int i=100;i<=n;i++) {
+				boolean check = true;
 				String str = ""+i;
-				
 				String [] strs = str.split("");
-				
-				
-				
-				
+				for(int j=0;j+2<strs.length;j++) {
+					if(Integer.parseInt(strs[j+2])-Integer.parseInt(strs[j+1])!=Integer.parseInt(strs[j+1])-Integer.parseInt(strs[j])) {					
+					check = false;						
+					}				
+				}	
+				if(check) {
+					ans +=1;
+				}
 			}
+			bw.write(""+ans);
+			bw.flush();
+			bw.close();		
 		}
-			
-		
-		
-		
-		
 	}
-
 }
